593516f8b7883300e538bcdc70db091b408cbf0a
Jesus Angulo <jesus.angulo@outlook.com>
2022-05-15 16:27:03 +0000

    Setup hot-reload for devcontainer

--- nextjs-sample/pages/_app.tsx
+++ nextjs-sample/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <div>
+      <Link href={'/products'} >Products</Link>
+    </div>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
