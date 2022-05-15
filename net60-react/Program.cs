a23aed46ac0d9a61f19f27636fbfe161009275dd
Jesus Angulo <jesus.angulo@outlook.com>
2022-05-15 16:55:52 +0000

    Mapping static routes from next.js

--- net60-react/Program.cs
+++ net60-react/Program.cs
@@ -22,6 +22,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");;
+app.MapFallbackToFile("/products","/products.html");
+app.MapFallbackToFile("/products/{pId}","/products/[productId].html");
+app.MapFallbackToFile("index.html");
 
 app.Run();
