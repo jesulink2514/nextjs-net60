b77cc1dfeff381b2f69f59289feb415b30f11bfa
Jesus Angulo <jesus.angulo@outlook.com>
2022-05-15 16:52:28 +0000

    Move api controller to /api route

--- net60-react/Controllers/WeatherForecastController.cs
+++ net60-react/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@
 namespace net60_react.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
