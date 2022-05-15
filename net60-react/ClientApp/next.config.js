8379bf661040db0ed1622f923998789cd112480c
Jesus Angulo <jesus.angulo@outlook.com>
2022-05-15 16:51:58 +0000

    Add proxy to next.js

--- net60-react/ClientApp/next.config.js
+++ net60-react/ClientApp/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const rewrites = () => {
+  return [
+    {
+      source: "/api/:path*",
+      destination: "http://localhost:5204/api/:path*",
+    },
+  ];
+};
 
 const nextConfig = {
   reactStrictMode: true,
@@ -8,7 +16,12 @@ const nextConfig = {
       aggregateTimeout: 300,
     }
     return config
-  }
+  },
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
+  rewrites,
 }
 
 module.exports = nextConfig
